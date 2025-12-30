84e261996ca72508cb44f07a4b601c0f95b381cd
Anshu Vij <vijanshu27@gmail.com>
2025-12-30 19:54:22 +0530

    basic game

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,22 +7,45 @@
 
 import SwiftUI
 
+
+struct Theme {
+    let name: String
+    let icon: String
+    let emojis: [String]
+    let color: Color
+}
+
 // contentView behaves like a view
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "🕷️", "😈", "✈️", "🕸️", "😱", "🍟", "🔥","😏"]
-    @State var cardCount: Int = 4
+    
+    @State var emojis = [String]()
+    @State var cardCount: Int = 0
+    @State var faceUpStates: [Bool] = []
+    @State var themeColor: Color =  .orange
+    
+    let themes: [Theme] = [
+        Theme(name: "Cars", icon: "car", emojis: ["🚙", "🏎️", "🚗", "🚕", "🚙", "🏎️", "🚗", "🚕"], color: .red),
+        Theme(name: "Smile", icon: "face.smiling", emojis: ["😀", "🤪", "😆", "😁", "😀", "🤪", "😆", "😁"], color: .blue),
+        Theme(name: "Default", icon: "restart.circle", emojis: ["👻", "🎃", "🕷️", "😈", "✈️","👻", "🎃", "🕷️", "😈", "✈️"], color: .orange)
+    ]
     
     var body: some View {
         VStack {
+            Text("Memorize!")
+                .font(.largeTitle)
+                .bold()
+            
             ScrollView {
                 cards
             }
             Spacer()
-            cardCountAdjusters
-           
+            // cardCountAdjusters
+            cardThemes
+            
         }
         .padding() // doesn't pass down to internal views
     }
+    
     func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
         Button {
             cardCount += offset
@@ -31,17 +54,23 @@ struct ContentView: View {
         }
         .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
-    
+    @ViewBuilder
     var cards: some View {
-        //LazyVGrid takes as minimum space as possible
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
-                    .aspectRatio(2/3, contentMode: .fit)
+        if emojis.count > 0 {
+            //LazyVGrid takes as minimum space as possible
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index], isFaceUp: $faceUpStates[index])
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
+                
             }
-            
+            .foregroundStyle(themeColor)
+        } else {
+            Text("Select a theme to start")
+                .font(.title)
+                .foregroundStyle(.red)
         }
-        .foregroundStyle(.orange)
     }
     
     var cardRemover: some View {
@@ -62,11 +91,37 @@ struct ContentView: View {
         .imageScale(.large)
         .font(.largeTitle)
     }
+    
+    var cardThemes: some View {
+        HStack {
+            ForEach(themes, id: \.name) { theme in
+                Spacer()
+                Button {
+                    emojis = theme.emojis.shuffled()
+                    cardCount = theme.emojis.count
+                    themeColor = theme.color
+                    faceUpStates = .init(repeating: false, count: cardCount)
+                } label: {
+                    VStack {
+                        Image(systemName: theme.icon)
+                        Text(theme.name)
+                            .font(.headline)
+                    }
+                }
+                Spacer()
+
+            }
+            
+        }
+        .font(.title)
+        .padding()
+        
+    }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp : Bool = true // @State keeps a pointer to isFaceUp thats why since pointer cant change but the isFaceUp can change
+    @Binding var isFaceUp : Bool
     
     var body: some View {
         ZStack {
@@ -92,5 +147,6 @@ struct CardView: View {
 
 
 #Preview {
-    ContentView()
+    ContentView(emojis: [String](), faceUpStates: [])
 }
+
